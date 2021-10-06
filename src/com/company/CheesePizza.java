5de13cf256f69bcc566788fe47a2ed0891c60cb0
dias <nurbergenovv.007@gmail.com>
2021-10-06 16:04:11 +0600

    Initial commit

---
+++ src/com/company/CheesePizza.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class CheesePizza extends Pizza {
+    public CheesePizza() {
+        name = “CheesePizza”;
+        dough = “Thin Crust Dough”;
+        sauce = “Marinara Sauce”;
+        toppings.add(“Grated Reggiano Cheese”);
+    }
+}
---
+++ src/com/company/PepperoniPizza.java
@@ -0,0 +1,4 @@
+package com.company;
+
+public class PepperoniPizza extends Pizza {
+}
